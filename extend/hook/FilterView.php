ab0a791bd16796473c4454a04fc5f4c44f4e5445
ryougimani <ryougimani@qq.com>
2017-08-29 17:02:40 +0800

    视图输出过滤

--- extend/hook/FilterView.php
+++ extend/hook/FilterView.php
@@ -20,22 +20,22 @@ class FilterView {
 
 	protected $request; // 当前请求对象
 
-    /**
-     * 行为入口
-     * @access public
-     * @param $params
-     */
-    public function run(&$params) {
-	    $this->request = Request::instance();
-	    list($appRoot, $uriSelf) = [$this->request->root(true), $this->request->url(true)];
-	    $uriRoot = strpos($appRoot, EXT) ? ltrim(dirname($appRoot), DS) : $appRoot;
-	    $uriStatic = "{$uriRoot}/static";
-	    $replace = [
-	    	'__APP__' => $appRoot,
-		    '__SELF__' => $uriSelf,
-		    '__PUBLIC__' => $uriRoot,
-		    '__STATIC__' => $uriStatic
-	    ];
-	    $params = str_replace(array_keys($replace), array_values($replace), $params);
-    }
+	/**
+	 * 行为入口
+	 * @access public
+	 * @param $params
+	 */
+	public function run(&$params) {
+		$this->request = Request::instance();
+		list($appRoot, $uriSelf) = [$this->request->root(true), $this->request->url(true)];
+		$uriRoot = strpos($appRoot, EXT) ? ltrim(dirname($appRoot), DS) : $appRoot;
+		$uriStatic = "{$uriRoot}/static";
+		$replace = [
+			'__APP__' => $appRoot,
+			'__SELF__' => $uriSelf,
+			'__PUBLIC__' => $uriRoot,
+			'__STATIC__' => $uriStatic
+		];
+		$params = str_replace(array_keys($replace), array_values($replace), $params);
+	}
 }
